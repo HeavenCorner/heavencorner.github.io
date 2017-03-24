4c06285cb7d484ce9687a50d87cf56e5c81b3da7
HeavenCorner <15649032317@163.com>
2017-03-24 13:36:01 +0800

    Site updated: 2017-03-24 13:36:01

--- js/blog.js
+++ js/blog.js
@@ -136,3 +136,10 @@ function scrollCheck(scrollTarget, toggleClass, scrollHeight){
         }, 500)
     }
 })();
+
+//标签云下下的a标签都绑定点击事件，跳转后在向下滑动一定的距离，20170324
+(function(){
+    $('#tag_cloud a').on('click',function(){
+        $(document).scroll($(document).scroll + 50);
+    })
+})();
